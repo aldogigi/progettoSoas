ee219bc82dc5cca41c28f63bd81981a76c39fef2
gianl <gianluca00_2013@libero.it>
2023-10-12 19:58:51 +0200

    LOGIN OAUTH

--- AutenticazioneOspedale/src/main/java/autenticazione.java
+++ AutenticazioneOspedale/src/main/java/autenticazione.java
@@ -1,12 +1,28 @@
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
 
 public class autenticazione extends JFrame {
+
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField EMail;
+	private JTextField Password;
 
 	/**
 	 * Launch the application.
@@ -29,11 +45,103 @@ public class autenticazione extends JFrame {
 	 */
 	public autenticazione() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 550, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+		JButton btnAutoLogin = new JButton("");
+		String iconfilePath = this.getClass().getClassLoader().getResource("Oauth_image.png").getFile();
+		btnAutoLogin.setIcon(new ImageIcon(iconfilePath));
+		btnAutoLogin.setBorder(BorderFactory.createEmptyBorder());
+		btnAutoLogin.setContentAreaFilled(false);
+		btnAutoLogin.setFocusable(false);
+		contentPane.add(btnAutoLogin);
+		btnAutoLogin.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.print("immagine cliccata");
+			}
+		});
+		
+		JLabel labelLogin = new JLabel(" LOGIN");
+		labelLogin.setHorizontalAlignment(SwingConstants.CENTER);
+		labelLogin.setFont(new Font("Times New Roman", Font.BOLD, 22));
+		
+		JLabel labelEmail = new JLabel("E-Mail");
+		labelEmail.setHorizontalAlignment(SwingConstants.CENTER);
+		labelEmail.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		
+		JLabel labelPassword = new JLabel("Password");
+		labelPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		labelPassword.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		
+		EMail = new JTextField();
+		EMail.setColumns(10);
+		
+		Password = new JTextField();
+		Password.setColumns(10);
+		
+		JButton btnAccedi = new JButton("Accedi");
+		btnAccedi.setBackground(Color.BLUE);
+		btnAccedi.setForeground(Color.WHITE);
+		
+		JButton btnRegistrati = new JButton("Registrati");
+		btnRegistrati.setBackground(Color.BLUE);
+		btnRegistrati.setForeground(Color.WHITE);
+		
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addComponent(labelEmail, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 526, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(labelPassword, GroupLayout.DEFAULT_SIZE, 516, Short.MAX_VALUE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(155)
+					.addComponent(btnAccedi, GroupLayout.DEFAULT_SIZE, 111, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnRegistrati, GroupLayout.DEFAULT_SIZE, 109, Short.MAX_VALUE)
+					.addGap(141))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(195)
+					.addComponent(btnAutoLogin, GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)
+					.addGap(180))
+				.addComponent(labelLogin, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 526, Short.MAX_VALUE)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(167)
+					.addComponent(Password, GroupLayout.DEFAULT_SIZE, 206, Short.MAX_VALUE)
+					.addGap(153))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(165)
+					.addComponent(EMail, GroupLayout.DEFAULT_SIZE, 206, Short.MAX_VALUE)
+					.addGap(155))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(labelLogin, GroupLayout.DEFAULT_SIZE, 45, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(labelEmail, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(EMail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(14)
+					.addComponent(labelPassword, GroupLayout.DEFAULT_SIZE, 45, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(Password, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(34)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnAccedi)
+						.addComponent(btnRegistrati))
+					.addGap(18)
+					.addComponent(btnAutoLogin, GroupLayout.DEFAULT_SIZE, 70, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		contentPane.setLayout(gl_contentPane);
+		setTitle("Login");
+		setResizable(false);
 	}
-
 }
